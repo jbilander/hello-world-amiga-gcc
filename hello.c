f08562557fd21e7c5dbe2a611bb49f33a74abd7e
jbilander <jorgen.bilander@gmail.com>
2023-06-08 12:56:18 +0200

    Updated file Indent using spaces tab size: 4

--- hello.c
+++ hello.c
@@ -3,17 +3,17 @@
 
 int main(int argc, char *argv[])
 {
-      struct ExecBase *SysBase;
-      struct Library *DOSBase;
+    struct ExecBase *SysBase;
+    struct Library *DOSBase;
 
-      SysBase = *(struct ExecBase **)4UL;
-      DOSBase = OpenLibrary((CONST_STRPTR) "dos.library", 0);
+    SysBase = *(struct ExecBase **)4UL;
+    DOSBase = OpenLibrary((CONST_STRPTR) "dos.library", 0);
 
-      if (DOSBase)
-      {
-            Write(Output(), "Hello world!\n", 13);
-            CloseLibrary(DOSBase);
-      }
+    if (DOSBase)
+    {
+        Write(Output(), "Hello world!\n", 13);
+        CloseLibrary(DOSBase);
+    }
 
-      return 0;
+    return 0;
 }
